60441491adaa1462f50a6a830d7c9c7fde3e3805
Altman <19309694020@163.com>
2026-09-04 11:09:20 +0800

    新增Alarm界面

---
+++ TrackLab.Client/View/AlarmViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackLab.UI.Common;
+
+namespace TrackLab.Client.View
+{
+    public class AlarmViewModel : ViewModelBase
+    {
+    }
+}
