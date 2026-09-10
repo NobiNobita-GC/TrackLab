3d6d05bd740e10318aa7133a37432080e927818e
Altman <19309694020@163.com>
2026-09-10 17:46:11 +0800

    不折腾主题

--- TrackLabClient/View/Configuration/HardwareMonitorViewModel.cs
+++ TrackLabClient/View/Configuration/HardwareMonitorViewModel.cs
@@ -101,17 +101,5 @@ namespace TrackLabClient.View.Configuration
 
             IOManager.Instance.UpdateDI(SelectedModule.Name, ioPoint.Name, !ioPoint.Value);
         }
-
-        public void SwitchTheme()
-        {
-            ResourceDictionary dictionary = Application.Current.Resources.MergedDictionaries[1];
-
-            dictionary.MergedDictionaries.Clear();
-
-            dictionary.MergedDictionaries.Add(new ResourceDictionary
-            {
-                Source = new Uri("/UI;component/Theme/Color/LightBlue.xaml", UriKind.Relative)
-            });
-        }
     }
 }
