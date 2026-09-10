bc05a344a9ce87a9fc7797280d9a950e74ad3ad6
Altman <19309694020@163.com>
2026-09-10 15:06:26 +0800

    DI/DO复用

--- Framework/UI/Control/Display/IODisplay.xaml.cs
+++ Framework/UI/Control/Display/IODisplay.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UI.Control.Display
 {
@@ -20,6 +9,14 @@ namespace UI.Control.Display
     /// </summary>
     public partial class IODisplay : UserControl
     {
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register(nameof(Items), typeof(IEnumerable), typeof(IODisplay));
+
+        public IEnumerable? Items
+        {
+            get => (IEnumerable?)GetValue(ItemsProperty);
+            set => SetValue(ItemsProperty, value);
+        }
         public IODisplay()
         {
             InitializeComponent();
