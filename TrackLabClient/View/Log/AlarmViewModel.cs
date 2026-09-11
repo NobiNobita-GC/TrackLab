16750c2a9dcfbae620ef67be3a2658a5c7971e86
Altman <19309694020@163.com>
2026-09-11 09:20:46 +0800

    添加报警列表基础数据

--- TrackLabClient/View/Log/AlarmViewModel.cs
+++ TrackLabClient/View/Log/AlarmViewModel.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using UI.Common;
 
-namespace TrackLabClient.View.Log
-{
-    public class AlarmViewModel : ViewModelBase
-    {
-    }
+namespace TrackLabClient.View.Log 
+{ 
+    public class AlarmViewModel : ViewModelBase 
+    { 
+        public ObservableCollection<string> Alarms { get; } = 
+            [
+            "Robot communication timeout", 
+            "LoadPort door open failed"
+            ]; 
+    } 
 }
