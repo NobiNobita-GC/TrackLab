d5cc2c205b2fcf58d20e13a58716360bff3ef82f
Altman <19309694020@163.com>
2026-09-11 09:50:55 +0800

    添加Core报警数据模型

--- TrackLabClient/View/Log/AlarmViewModel.cs
+++ TrackLabClient/View/Log/AlarmViewModel.cs
@@ -1,14 +1,28 @@
-﻿using System.Collections.ObjectModel;
+﻿using Core.Alarm;
+using System.Collections.ObjectModel;
 using UI.Common;
 
-namespace TrackLabClient.View.Log 
-{ 
-    public class AlarmViewModel : ViewModelBase 
-    { 
-        public ObservableCollection<string> Alarms { get; } = 
-            [
-            "Robot communication timeout", 
-            "LoadPort door open failed"
-            ]; 
-    } 
+namespace TrackLabClient.View.Log
+{
+    public class AlarmViewModel : ViewModelBase
+    {
+        public ObservableCollection<AlarmItem> Alarms { get; } =
+        [
+            new AlarmItem
+            {
+                Time = "09:20:15",
+                Module = "Robot",
+                Level = "Error",
+                Message = "Communication timeout"
+            },
+
+            new AlarmItem
+            {
+                Time = "09:21:32",
+                Module = "LoadPort",
+                Level = "Warning",
+                Message = "Door open failed"
+            }
+        ];
+    }
 }
