83342ff034611f926ba0d3de9b55b905591cfdea
Altman <19309694020@163.com>
2026-09-11 09:30:48 +0800

    添加Core报警数据模型

---
+++ Framework/Core/Alarm/AlarmItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Alarm
+{
+    public class AlarmItem
+    {
+        public string Time { get; set; } = "";
+        public string Module { get; set; } = "";
+        public string Level { get; set; } = "";
+        public string Message { get; set; } = "";
+    }
+}
