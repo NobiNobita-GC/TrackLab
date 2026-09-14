1223ef2635c5a5231bcdf98f0102d0dcb7f17406
Nobita <19309694020@163.com>
2026-09-14 16:57:33 +0800

    添加语言管理器

---
+++ Framework/Core/Language/LanguageManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace Core.Language
+{
+    public static class LanguageManager
+    {
+        private static string _languageType = "en";
+
+        private static readonly ResourceManager resourceManager = 
+            new("Core.Language.Lang-Word", typeof(LanguageManager).Assembly);
+
+        public static event Action? LanguageChanged;
+
+        public static string GetString(string key)
+        {
+            key = key.Replace(" ", string.Empty);
+            string? value = resourceManager.GetString(key, new CultureInfo(_languageType));
+            return string.IsNullOrEmpty(value) ? key : value;
+        }
+
+        public static void ChangeLanguage(string cultureCode)
+        {
+            if (cultureCode.Equals("cn", StringComparison.OrdinalIgnoreCase)
+                || cultureCode.Equals("zh-CN", StringComparison.OrdinalIgnoreCase))
+            {
+                _languageType = "zh-CN";
+            }
+            else
+            {
+                _languageType = "en";
+            }
+            LanguageChanged?.Invoke();       
+        }
+    }
+}
