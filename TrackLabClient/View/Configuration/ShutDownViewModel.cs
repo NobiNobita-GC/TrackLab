4ea0333c25690358a678c67e967b13a27060d146
Nobita <19309694020@163.com>
2026-09-14 14:54:27 +0800

    实现程序关闭功能

--- TrackLabClient/View/Configuration/ShutDownViewModel.cs
+++ TrackLabClient/View/Configuration/ShutDownViewModel.cs
@@ -1,8 +1,13 @@
-﻿using UI.Common;
+﻿using System.Windows;
+using UI.Common;
 
 namespace TrackLabClient.View.Configuration
 {
     public class ShutDownViewModel : ViewModelBase
     {
+        public void Shutdown()
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
