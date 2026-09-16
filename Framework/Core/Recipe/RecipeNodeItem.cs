1c83c1230ba039efaa0ea3aa0e1181463beab6ea
Nobita <19309694020@163.com>
2026-09-16 15:49:01 +0800

    支持配方文件夹节点初始化

---
+++ Framework/Core/Recipe/RecipeNodeItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Core.Recipe
+{
+    public class RecipeNodeItem
+    {
+        public bool IsFolder { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string FullPath { get; set; } = string.Empty;
+
+        public RecipeData? RecipeData { get; set; }
+
+        public RecipeNodeItem? Parent { get; set; }
+
+        public ObservableCollection<RecipeNodeItem> SubNodes { get; set; } = [];
+
+        public RecipeNodeItem()
+        {
+        }
+
+        public RecipeNodeItem(DirectoryInfo directory, RecipeNodeItem? parent)
+        {
+            Name = directory.Name;
+            FullPath = directory.FullName;
+            Parent = parent;
+            IsFolder = true;
+        }
+    }
+}
