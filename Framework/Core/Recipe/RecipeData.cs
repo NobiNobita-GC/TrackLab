979b62d0b227669c2d59539caa120b9adccd1458
Nobita <19309694020@163.com>
2026-09-16 15:40:24 +0800

    添加配方数据模型

--- Framework/Core/Recipe/RecipeData.cs
+++ Framework/Core/Recipe/RecipeData.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Text.Json;
 
 namespace Core.Recipe
 {
@@ -7,5 +8,51 @@ namespace Core.Recipe
         public Dictionary<string, string> Header { get; set; } = [];
         public ObservableCollection<Dictionary<string, string>> Step { get; set; } = [];
         public Dictionary<string, string> Config { get; set; } = [];
+
+        public string ToJsonString()
+        {
+            JsonSerializerOptions options = new()
+            {
+                WriteIndented = true,
+            };
+
+            return JsonSerializer.Serialize(this, options);
+        }
+
+        public static RecipeData FromJsonString(string recipeContent)
+        {
+            if (string.IsNullOrEmpty(recipeContent))
+            {
+                return new RecipeData();
+            }
+
+            return JsonSerializer.Deserialize<RecipeData>(recipeContent);
+        }
+
+        public void Copy(RecipeData? source)
+        {
+            if (source == null)
+            {
+                return;
+            }
+
+            Header.Clear();
+            foreach (var item in source.Header)
+            {
+                Header.Add(item.Key, item.Value);
+            }
+
+            Step.Clear();
+            foreach (var item in source.Step)
+            {
+                Step.Add(new Dictionary<string, string>(item));
+            }
+
+            Config.Clear();
+            foreach (var item in source.Config)
+            {
+                Config.Add(item.Key, item.Value);
+            }
+        }
     }
 }
