09ae9ccab7d6d93c5cfcdc5d4d6be695776beb64
Nobita <19309694020@163.com>
2026-09-16 15:00:05 +0800

    添加配方数据模型

---
+++ Framework/Core/Recipe/RecipeData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Core.Recipe
+{
+    public class RecipeData
+    {
+        public Dictionary<string, string> Header { get; set; } = [];
+        public ObservableCollection<Dictionary<string, string>> Step { get; set; } = [];
+        public Dictionary<string, string> Config { get; set; } = [];
+    }
+}
