a365bf3fc15d3ef832e631a3d272e525ef63d46c
Nobita <19309694020@163.com>
2026-09-17 14:37:54 +0800

    完善配方节点路径查找

--- Framework/Core/Recipe/RecipeManager.cs
+++ Framework/Core/Recipe/RecipeManager.cs
@@ -30,12 +30,15 @@ namespace Core.Recipe
             RecipeNodeItem? findNode = _recipeNodeItem;
             string[] paths = path.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries);
 
-            for(int i = 0; i < paths.Length; i++)
+            for (int i = 0; i < paths.Length; i++)
             {
                 string item = paths[i];
+
+                //配方节点文件
                 bool isLastItem = i == paths.Length - 1;
 
-                findNode = findNode?.SubNodes.FirstOrDefault(n => n.Name.Equals(item, StringComparison.OrdinalIgnoreCase));
+                //配方文件节点，而不是碰巧同名的文件夹
+                findNode = findNode?.SubNodes.FirstOrDefault(n => n.Name.Equals(item, StringComparison.OrdinalIgnoreCase) && n.IsFolder != isLastItem);
 
                 if (findNode == null)
                 {
