7b2a5fcdb23f157b2b26bdf416a1aba71196faca
Nobita <19309694020@163.com>
2026-09-17 16:31:19 +0800

    添加配方管理界面

---
+++ TrackLabClient/View/Recipe/RecipeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TrackLabClient.View.Recipe
+{
+    /// <summary>
+    /// RecipeView.xaml 的交互逻辑
+    /// </summary>
+    public partial class RecipeView : UserControl
+    {
+        public RecipeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TrackLabClient/View/Recipe/RecipeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Core.Recipe;
+using System.Collections.ObjectModel;
+using UI.Common;
+
+namespace TrackLabClient.View.Recipe
+{
+    public class RecipeViewModel : ViewModelBase
+    {
+        public ObservableCollection<RecipeNodeItem> RecipeNodes { get; }
+
+        public string RootPath => RecipeManager.Instance.GetRootPath();
+
+        public int RecipeCount => RecipeManager.Instance.FindAllRecipe().Count();
+
+        public RecipeViewModel()
+        {
+            RecipeNodes = RecipeManager.Instance.GetRecipes();
+        }
+    }
+}
