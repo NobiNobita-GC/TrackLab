91ace47d25f624f5882062d6ab5d8429f5beaf18
Nobita <19309694020@163.com>
2026-09-17 11:31:06 +0800

    支持按路径查找配方节点

--- Framework/Core/Recipe/RecipeManager.cs
+++ Framework/Core/Recipe/RecipeManager.cs
@@ -25,6 +25,26 @@ namespace Core.Recipe
             return _recipeNodeItem.SubNodes;
         }
 
+        public RecipeNodeItem? FindNodeByPath(string path)
+        {
+            RecipeNodeItem? findNode = _recipeNodeItem;
+            string[] paths = path.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries);
+
+            for(int i = 0; i < paths.Length; i++)
+            {
+                string item = paths[i];
+                bool isLastItem = i == paths.Length - 1;
+
+                findNode = findNode?.SubNodes.FirstOrDefault(n => n.Name.Equals(item, StringComparison.OrdinalIgnoreCase));
+
+                if (findNode == null)
+                {
+                    break;
+                }
+            }
+            return findNode;
+        }
+
         public string GetRootPath()
         {
             return _rootPath;
--- Framework/Core/Recipe/RecipeNodeItem.cs
+++ Framework/Core/Recipe/RecipeNodeItem.cs
@@ -5,10 +5,11 @@ namespace Core.Recipe
     public class RecipeNodeItem
     {
         public bool IsFolder { get; set; }
-
         public string Name { get; set; } = string.Empty;
-
         public string FullPath { get; set; } = string.Empty;
+        public string Path => GetRecipePath();
+        public string PathWithType => GetPathWithType();
+        public string RecipeType => GetRecipeType();
 
         public RecipeData? RecipeData { get; set; }
 
@@ -16,6 +17,38 @@ namespace Core.Recipe
 
         public ObservableCollection<RecipeNodeItem> SubNodes { get; set; } = [];
 
+        private string GetPathWithType()
+        {
+            string path = System.IO.Path.GetRelativePath(RecipeManager.Instance.GetRootPath(), FullPath);
+
+            if (!IsFolder)
+            {
+                path = System.IO.Path.ChangeExtension(path, null) ?? string.Empty;
+            }
+
+            return path;
+        }
+
+        private string GetRecipeType()
+        {
+            string path = GetPathWithType();
+            string[] paths = path.Split(System.IO.Path.DirectorySeparatorChar);
+
+            return paths.FirstOrDefault() ?? string.Empty;
+        }
+
+        private string GetRecipePath()
+        {
+            string path = GetPathWithType();
+            int index = path.IndexOf(System.IO.Path.DirectorySeparatorChar);
+
+            if (index < 0)
+            {
+                return string.Empty;
+            }
+
+            return path.Substring(index + 1);
+        }
         public RecipeNodeItem()
         {
         }
@@ -40,7 +73,7 @@ namespace Core.Recipe
 
         public RecipeNodeItem(FileInfo file, RecipeNodeItem parent)
         {
-            Name = Path.GetFileNameWithoutExtension(file.Name);
+            Name = System.IO.Path.GetFileNameWithoutExtension(file.Name);
             FullPath = file.FullName;
             Parent = parent;
             IsFolder = false;
